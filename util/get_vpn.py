57ceb1b8eebce038f255540dff7e6a78d8a697c5
longweiqiang <573925242@qq.com>
2019-10-16 20:22:40 +0800

    优化网络连接判断

--- util/get_vpn.py
+++ util/get_vpn.py
@@ -7,24 +7,33 @@
 # @Software: PyCharm
 
 import os
+import platform
 from util.Logger import logger as log
 
 
 def start_vpn():
-    print('-------------------------------------------------------')
-    print("正在启动VPN:连接KA网络")
-    log.info("正在启动VPN:连接KA网络")
-    print(os.system('rasdial "ka-dev" "KAprivate" "KAprivate@$^135"'))
-    log.info(os.system('rasdial "ka-dev" "KAprivate" "KAprivate@$^135"'))
-    print("VPN连接成功:KA网络")
-    log.info("VPN连接成功:KA网络")
+    if platform.system() == 'Windows':
+        log.info("当前系统为Windows，正在连接VPN")
+        # print('-------------------------------------------------------')
+        # print("正在启动VPN:连接KA网络")
+        log.info("正在启动VPN:连接KA网络")
+        # print(os.system('rasdial "ka-dev" "KAprivate" "KAprivate@$^135"'))
+        log.info(os.system('rasdial "ka-dev" "KAprivate" "KAprivate@$^135"'))
+        # print("VPN连接成功:KA网络")
+        log.info("VPN连接成功:KA网络")
+    else:
+        log.info("当前系统为Liunx，不连接VPN")
 
 
 def stop_vpn():
-    print('-------------------------------------------------------')
-    print("正在关闭VPN:关闭KA网络")
-    log.info("正在关闭VPN:关闭KA网络")
-    print(os.system('rasdial ka-dev /disconnect'))
-    log.info(os.system('rasdial ka-dev /disconnect'))
-    print("VPN关闭成功:KA网络")
-    log.info("VPN关闭成功:KA网络")
+    if platform.system() == 'Windows':
+        log.info("当前系统为Windows，正在连接VPN")
+        # print('-------------------------------------------------------')
+        # print("正在关闭VPN:关闭KA网络")
+        log.info("正在关闭VPN:关闭KA网络")
+        # print(os.system('rasdial ka-dev /disconnect'))
+        log.info(os.system('rasdial ka-dev /disconnect'))
+        # print("VPN关闭成功:KA网络")
+        log.info("VPN关闭成功:KA网络")
+    else:
+        log.info("当前系统为Liunx，不连接VPN")
